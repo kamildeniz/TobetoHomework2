c13f67a22ef23b1f1a7907f54a5183699f16e443
kamil07deniz <kamil07deniz@gmail.com>
2023-10-25 21:18:31 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Classes;
+
+Console.WriteLine("Hello, World!");
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add();
+customerManager.Update();
+ProductManager productManager = new ProductManager();
+productManager.Add();
+productManager.Update();
